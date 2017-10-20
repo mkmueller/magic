744ef235c1a7e4c4b24c11b7b6b403c07ce81e0c
Mark K Mueller <mark@markmueller.com>
2017-10-20 13:18:18 -0700

    Updated signatures and documentation. FireInfo.Ext is now an array.

--- magic.go
+++ magic.go
@@ -1,15 +1,21 @@
 // Copyright (c) 2017 Mark K Mueller, (markmueller.com), All rights reserved.
 
-// Package magic will determine the type of file based on it's magic-bytes. This
-// package will identify the most common types of files. There are a kbutt load of
-// signatures at http://www.garykessler.net/library/file_sigs.html
+// Package magic will determine the type of file based on the first "magic
+// bytes". The file signatures are not complete, but represent some of the most
+// common file types.
 //
 package magic
 
 import (
-	"os"
 	"errors"
+	"os"
 )
+
+// Byte array
+type ba []byte
+
+// String array
+type sa []string
 
 // File signature struct
 type fs struct {
@@ -18,15 +24,18 @@ type fs struct {
 	Description string
 
 	// Comma delimited list of known extensions
-	Ext			string
+	Ext []string
 
 	// Offset where the signature begins
-	Offset		int
+	Offset int
 
 	// Signature of file type
-	Signature	[]byte
+	Signature []byte
 
 }
+
+// Signature table
+type st []fs
 
 // FileInfo struct
 type FileInfo struct {
@@ -35,22 +44,33 @@ type FileInfo struct {
 	Name string
 
 	// Comma delimited list of known extensions for this file type
-	Ext			string
+	Ext []string
 
 	// Description of the file
 	Description string
 
 	// Size of the file in bytes
 	Size int64
+}
+
+var n_bytes int64
+
+func init() {
+
+	// set n_bytes to longest signature and offset
+	for _, sig := range signatureTable {
+		x := int64(sig.Offset + len(sig.Signature))
+		if x > n_bytes {
+			n_bytes = x
+		}
+	}
 
 }
 
-const n_bytes = 8
-
 // FileBytes returns a FileInfo struct. The error will be non-nil if the file
-// type cannot be determined.
+// type cannot be determined or if the file cannot be read.
 //
-func FileBytes ( file string ) (FileInfo, error) {
+func FileBytes(file string) (FileInfo, error) {
 
 	var fi FileInfo
 	var err error
@@ -62,7 +82,7 @@ func FileBytes ( file string ) (FileInfo, error) {
 	}
 
 	// Check file exists
-	fs,err := os.Stat(file)
+	fs, err := os.Stat(file)
 	if err != nil {
 		err = errors.New("File does not exist")
 		return fi, err
@@ -76,55 +96,63 @@ func FileBytes ( file string ) (FileInfo, error) {
 
 	// Check file size
 	size := fs.Size()
-	if size < n_bytes {
-		err = errors.New("File too small to test")
+	if size == 0 {
+		err = errors.New("Empty file")
 		return fi, err
 	}
 
+	// set read_len to the smaller of n_bytes or file size
+	read_len := n_bytes
+	if size < read_len {
+		read_len = size
+	}
+
 	// Open file for reading
-	f,err := os.Open(file)
+	f, err := os.Open(file)
 	if err != nil {
 		return fi, err
 	}
 	defer f.Close() // always
 
-	// Read first n_bytes
-	byts := make([]byte, n_bytes)
-	_,err = f.Read(byts)
+	// Read first read_len bytes
+	byts := make([]byte, read_len)
+	_, err = f.Read(byts)
 	if err != nil {
 		return fi, err
 	}
 
 	// Lookup bytes in signature table
-	if desc,ext := lookup(byts); desc != "" {
+	if desc, ext := lookup(byts); desc != "" {
 		fi.Description = desc
 		fi.Name = file
-		fi.Ext  = ext
+		fi.Ext = ext
 		fi.Size = size
 		return fi, err
 	}
 
-	err = errors.New("Unknown")
+	err = errors.New("Unknown file type")
 	return fi, err
 }
 
-func lookup ( b []byte ) (string,string) {
-	for _,sig := range SignatureTable {
+func lookup(b []byte) (string, []string) {
+	for _, sig := range signatureTable {
 		ln := len(sig.Signature)
 		offset := sig.Offset
-		if equal( b[offset:ln], sig.Signature ) {
-			return sig.Description,sig.Ext
+		if equal(b[offset:ln], sig.Signature) {
+			return sig.Description, sig.Ext
 		}
 	}
-	return "",""
+	return "", nil
 }
 
-func equal ( a1, a2 []byte ) bool {
-	for i,_ := range a1 {
+func equal(a1, a2 []byte) bool {
+	if len(a1) != len(a2) {
+		return false
+	}
+	for i, _ := range a1 {
 		if a1[i] != a2[i] {
 			return false
 		}
 	}
 	return true
 }
-
--- magic_test.go
+++ magic_test.go
@@ -3,106 +3,116 @@
 package magic_test
 
 import (
-	"os"
 	"fmt"
-	"testing"
-	"io/ioutil"
 	"github.com/mkmueller/magic"
-    . "github.com/smartystreets/goconvey/convey"
+	. "github.com/smartystreets/goconvey/convey"
+	"io/ioutil"
+	"os"
+	"testing"
 )
 
 var tiniestGifEver string
 
-func TestMain ( m *testing.M ) {
+func TestMain(m *testing.M) {
 
+	// Create the Tiniest GIF Ever.
+	// Horked from http://probablyprogramming.com/2009/03/15/the-tiniest-gif-ever
 	tiniestGifEver = createTestFile([]byte{0x47, 0x49, 0x46, 0x38, 0x39, 0x61,
-								0x01, 0x00, 0x01, 0x00, 0x00, 0xFF, 0x00,
-								0x2C, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00,
-								0x01, 0x00, 0x00, 0x02, 0x00, 0x3B})
+		0x01, 0x00, 0x01, 0x00, 0x00, 0xFF,
+		0x00, 0x2C, 0x00, 0x00, 0x00, 0x00,
+		0x01, 0x00, 0x01, 0x00, 0x00, 0x02,
+		0x00, 0x3B})
 
+	// Wait here for my tests to complete
 	returncode := m.Run()
 
-	os.Remove(tiniestGifEver)
+	// Clean up
 	os.Exit(returncode)
 
 }
 
 // Example FileBytes
-func ExampleFileBytes () {
+func ExampleFileBytes() {
 
-	// Thanks to ProbablyProgramming.com for The Tiniest Gif Ever
+	// Create a tiny GIF image file
 	fi, err := magic.FileBytes(tiniestGifEver)
 	if err == nil {
-		fmt.Printf( "The file type is %s and the extension should probably be %s",  fi.Description, fi.Ext )
+		fmt.Printf("The file type is %s", fi.Description)
 	}
-	// Output: The file type is GIF89a Image and the extension should probably be gif
+	// Output: The file type is GIF89a Image
 
 }
 
-func TestFileBytes ( t *testing.T ) {
+func TestFileBytes(t *testing.T) {
 
-    Convey("Test The Tiniest GIF Ever", t, func() {
+	Convey("Test unknown file", t, func() {
 
-		fi,err := magic.FileBytes(tiniestGifEver)
-		So( err, 			ShouldEqual, nil )
-		So( fi.Description,	ShouldEqual, "GIF89a Image" )
+		fi, err := magic.FileBytes(tiniestGifEver)
+		So(err, ShouldEqual, nil)
+		So(fi.Description, ShouldEqual, "GIF89a Image")
+		So(fi.Ext[0], ShouldEqual, "gif")
 
-    })
-
-}
-
-func TestFileBytes_errors ( t *testing.T ) {
-
-    Convey("Test file name", t, func() {
-
-		_,err := magic.FileBytes("")
-		So( err.Error(),	ShouldEqual, "File name not supplied" )
-
-    })
-
-    Convey("Test non-existent file", t, func() {
-
-		_,err := magic.FileBytes("./ thils file should not exist . really")
-		So( err.Error(),	ShouldEqual, "File does not exist" )
-
-    })
-
-    Convey("Test using directory name", t, func() {
-
-		_,err := magic.FileBytes("..")
-		So( err.Error(),	ShouldEqual, "Directory" )
-
-    })
-
-    Convey("Test using a file too small", t, func() {
-
-		testfile := createTestFile([]byte{0x47, 0x49, 0x46, 0x38, 0x39})
-
-		_,err := magic.FileBytes(testfile)
-		So( err.Error(),	ShouldEqual, "File too small to test" )
-
-		os.Remove(testfile)
-
-    })
-
-    Convey("Test an unknown file type", t, func() {
-
-		testfile := createTestFile([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-										  0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
-
-		_,err := magic.FileBytes(testfile)
-		So( err.Error(),	ShouldEqual, "Unknown" )
-
-		os.Remove(testfile)
-
-    })
+	})
 
 }
 
+func TestFileBytes_errors(t *testing.T) {
 
+	unknownFile := createTestFile([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
 
+	emptyFile := createTestFile(nil)
 
-func createTestFile ( data []byte ) string {
+	Convey("Test file name", t, func() {
+
+		_, err := magic.FileBytes("")
+		So(err.Error(), ShouldEqual, "File name not supplied")
+
+	})
+
+	Convey("Test non-existent file", t, func() {
+
+		_, err := magic.FileBytes("./hopefully, this file does not really exist")
+		So(err.Error(), ShouldEqual, "File does not exist")
+
+	})
+
+	Convey("Test using directory name", t, func() {
+
+		_, err := magic.FileBytes("..")
+		So(err.Error(), ShouldEqual, "Directory")
+
+	})
+
+	Convey("Create an empty file", t, func() {
+
+		_, err := magic.FileBytes(emptyFile)
+		So(err.Error(), ShouldEqual, "Empty file")
+
+	})
+
+	Convey("Test an unknown file type", t, func() {
+
+		_, err := magic.FileBytes(unknownFile)
+		So(err.Error(), ShouldEqual, "Unknown file type")
+
+	})
+
+	// Clean up
+	os.Remove(unknownFile)
+	os.Remove(emptyFile)
+
+}
+
+func createTestFile(data []byte) string {
 
 	fl, err := ioutil.TempFile("/tmp", "GOTEST_")
 	if err != nil {
@@ -111,7 +121,7 @@ func createTestFile ( data []byte ) string {
 	}
 	defer fl.Close()
 
-	_,err = fl.Write(data)
+	_, err = fl.Write(data)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
